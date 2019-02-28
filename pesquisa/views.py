298b8d18bbf06d2a1d69a75db1219ebf6edbea62
Renan Rodrigo <renanrb@ime.usp.br>
2019-02-27 21:48:57 -0300

    Build the index view with the questions

--- pesquisa/views.py
+++ pesquisa/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from .models import Pergunta
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    todas = Pergunta.objects.all()
+    return render(request, 'index.html', {
+        "perguntas": todas
+    })
